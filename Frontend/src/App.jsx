814d7ef15d100c1143d50a74670f538667100048
Sohaib Nadeem <sohaibmr158@gmail.com>
2026-08-08 23:16:24 +0500

    FinaldataPush

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import * as faceapi from "face-api.js";
 
-const API_BASE_URL = "http://localhost:3000";
-
+// const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 async function getTracksForMood(mood) {
   try {
     const response = await fetch(`${API_BASE_URL}/songs?mood=${mood}`, {
